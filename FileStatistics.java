e911d88a684d747c69b44af61305726eea99c106
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-11-28 19:06:50 -0600

    Update FileStatistics.java

--- FileStatistics.java
+++ FileStatistics.java
@@ -4,20 +4,18 @@ import java.io.IOException;
 
 public class FileStatistics {
 	public static void main (String args[]){
-	Path userFile = Paths.get("SomeFolder/sample.txt");
-	
-	try{
-	BasicFileAttributes attr = Files.readAttributes(userFile, BasicFileAttributes.class);
-	
-	System.out.println("Files name: " + userFile.getFileName());
-	System.out.println("Files containing folder: " + userFile.toString());
-	System.out.println("Files size: " + attr.size());
-	System.out.println("Files time of modification: " + attr.lastModifiedTime());
-	}
-	catch(IOException e)
-	{
-	System.out.println("Something went wrong");
-	}
-	
+		Path userFile = Paths.get("SomeFolder/sample.txt");
+		
+		try{
+			BasicFileAttributes attr = Files.readAttributes(userFile, BasicFileAttributes.class);
+			
+			System.out.println("Files name: " + userFile.getFileName());
+			System.out.println("Files containing folder: " + userFile.toString());
+			System.out.println("Files size: " + attr.size());
+			System.out.println("Files time of modification: " + attr.lastModifiedTime());
+		}
+		catch(IOException e){
+			System.out.println("Something went wrong");
+		}
 	}
 }
